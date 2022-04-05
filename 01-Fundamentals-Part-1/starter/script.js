1702ac34861e9d59d32d01fb4bfae9794e5bebbb
Ryan Crespo <crespoffi@gmail.com>
2022-04-05 17:53:03 -0400

    working on assignments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,10 +5,18 @@ console.log('Ryan');
 console.log(23);
 
 let firstName = 'Ryan';
-let first = 'Joe';
-let firstNamePerson
-let first_name
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let person = 'ryan';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
