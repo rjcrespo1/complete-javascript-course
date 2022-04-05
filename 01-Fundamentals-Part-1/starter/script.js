97a65ea192cf3eb9ae9fd0fbd35eb0174ef6be38
Ryan Crespo <crespoffi@gmail.com>
2022-04-04 22:21:00 -0400

    variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Ryan');
+console.log(23);
+
+let firstName = 'Ryan';
+let first = 'Joe';
+let firstNamePerson
+let first_name
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
