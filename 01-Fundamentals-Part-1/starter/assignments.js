36d273f8e22470e037f3c5cf85e55134e79817ba
Ryan Crespo <crespoffi@gmail.com>
2022-04-05 23:18:21 -0400

    worked on assignments

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -69,4 +69,4 @@ if (language === 'english' && population < 50 && !isIsland) {
     console.log(`You should live in ${country} :)`);
 } else {
     console.log(`${country} does not meet your criteria :(`)
-}
+};
